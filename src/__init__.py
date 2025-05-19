e4481a2b9688d8fdc6019b0274ea2783f7d7ac87
Alexandre Jr Miranda <49948786+alejuniormsn@users.noreply.github.com>
2025-05-19 09:47:43 -0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 import cx_Oracle
 
 def get_connection():
-    return cx_Oracle.connect("globus/glbett2012@192.168.0.5:1521/oracle")
+    return cx_Oracle.connect("USER/PASSWORD@192.168.0.5:1521/oracle")
